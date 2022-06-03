ed393f8cc18b064ef884dde9191f5a4bd186b640
Damir2302 <damir23.02@mail.ru>
2022-06-03 18:17:46 +0300

    header

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
 
     // Menu
     const $nav = $('.header__menu')
-    const $btn = $('.hidden-list span')
+    const $btn = $('.hidden-menu span')
     const $vList = $('.header__menu-list')
-    const $hList = $('.hidden-list ul')
-    const $hListMenu = $('.hidden-list')
+    const $hList = $('.hidden-menu ul')
+    const $hListMenu = $('.hidden-menu')
     const breaks = []
 
     function updateNav() {
-        let availableSpace = $nav.width() - $btn.width() - 30.5;
+        let availableSpace = $nav.width() - $btn.width() - 65;
         if ($vList.width() > availableSpace) {
             breaks.push($vList.width())
             $vList.children().last().prependTo($hList)
@@ -51,6 +51,55 @@ $(document).ready(function() {
 
     if ($(window).width() >= 744) {
         updateNav()
+    }
+
+    // Navbar
+    const $navbar = $('.navbar__inner')
+    const $navbarBtn = $('.hidden-navbar span')
+    const $nav_vList = $('.navbar__menu')
+    const $nav_hList = $('.hidden-navbar .hidden-list')
+    const $nav_hListMenu = $('.hidden-navbar')
+    const nav_breaks = []
+
+    function updateNavbar() {
+        let availableSpace;
+        if ($(window).width() >= 744 && $(window).width() < 768) {
+            availableSpace = $navbar.width() - $navbarBtn.width() - 10;
+        } else if ($(window).width() >= 786) {
+            availableSpace = $navbar.width() - $navbarBtn.width() - 85;
+        }
+        if ($nav_vList.width() > availableSpace) {
+            nav_breaks.push($nav_vList.width())
+            $nav_vList.children().last().prependTo($nav_hList)
+
+            if ($nav_hListMenu.hasClass('hidden')) {
+                $nav_hListMenu.removeClass('hidden')
+            }
+
+        } else {
+            if (availableSpace > nav_breaks[nav_breaks.length - 1]) {
+              $nav_hList.children().first().appendTo($nav_vList)
+              nav_breaks.pop()
+            }
+
+            if (nav_breaks.length < 1) {
+                $nav_hListMenu.addClass('hidden');
+            }
+          }
+
+        if ($nav_vList.width() > availableSpace) {
+            updateNavbar();
+        }
+    }
+
+    $(window).resize(function() {
+        if ($(window).width() >= 744) {
+            updateNavbar()
+        }
+    })
+
+    if ($(window).width() >= 744) {
+        updateNavbar()
     }
 
 });
