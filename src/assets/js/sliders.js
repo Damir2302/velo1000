b1190758163d170a022315b1b8ef52e8d75f18d0
Damir2302 <damir23.02@mail.ru>
2022-06-10 17:45:17 +0300

    fixed

--- src/assets/js/sliders.js
+++ src/assets/js/sliders.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
 
     let bannerSlider = new Swiper(".banner__slider", {
         pagination: {
-          el: ".swiper-pagination",
+          el: ".banner__slider .swiper-pagination",
+        }
+    });
+
+    let brandsSlider = new Swiper(".brands__slider", {
+        slidesPerView: 'auto',
+        freeMode: true,
+        loop: true,
+
+        breakpoints: {
+          744: {
+            centeredSlides: true,
+          }
         }
     });
 
